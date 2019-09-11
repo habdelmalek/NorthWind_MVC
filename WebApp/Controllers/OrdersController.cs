1442093071b4075a2782a65a57fb0aa195bf460f
habdelmalek <hanishawky_3@hotmail.com>
2019-09-11 14:15:56 -0400

    added emps

---
+++ WebApp/Controllers/OrdersController.cs
@@ -0,0 +1,23 @@
+﻿using DataAccessLayer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApp.Controllers
+{
+    public class OrdersController : Controller
+    {
+        // GET: Orders
+        public ActionResult Index()
+        {
+            var unitOfWork = new UnitOfWork();
+
+            var emps = unitOfWork.EmployeeRepository.GetAll().Select(emp => emp.FirstName + " " + emp.LastName).ToList();
+
+
+            return View(emps);
+        }
+    }
+}
